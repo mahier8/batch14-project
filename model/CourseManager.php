051e4656907db06ea3e7b7decf27facf793ab496
Mahier <yonhubby@Mahiers-MacBook-Pro.local>
2021-12-08 11:15:45 +0900

    creation of the managers

---
+++ model/CourseManager.php
@@ -0,0 +1,7 @@
+<?php
+class CourseManager extends Manager {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ model/UserManager.php
@@ -0,0 +1,7 @@
+<?php
+class UserManager extends Manager {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
