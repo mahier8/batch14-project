32d6691b5fab5e99ead50f3ac5267ff0e2eac302
= <martinrombach88@gmail.com>
2021-12-10 11:06:29 +0900

    Minor edit to CourseManager.php

--- model/CourseManager.php
+++ model/CourseManager.php
@@ -8,7 +8,7 @@ class CourseManager extends Manager {
 
     public function getCourses() {
     //If you want to use this query with a specific id, you need to convert to a prepare/execute/bind param structure
-        $response = $this->_connexion->query("SELECT id, image, name, teacher, dayTime, nbStudents FROM course_test ORDER BY id");
+        $response = $this->_connexion->query("SELECT id, image, name, teacher, dayTime, nbStudents FROM course ORDER BY id");
         $courses = $response->fetchAll(\PDO::FETCH_ASSOC);
         $response->closeCursor();
         return $courses;
